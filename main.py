883dbd6bd899c2abc64c6078ab09ef6c7c88e8d9
Deanna <57398374+dpiedra42@users.noreply.github.com>
2022-08-25 15:43:54 +0000

    new changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import random, pygame, sys
+import pygame, sys, random
 from pygame.locals import *
-from player import *
-from platforms import *
+from player import Player
+from platforms import Platforms
 
 pygame.init()
 screen_info = pygame.display.Info()
@@ -19,63 +19,63 @@ player = ''
 
 
 def get_player_actions():
-  p1_actions = {}
-  # create a dictionary of all player images
-  p1_actions["p1_jump"] = pygame.image.load("images/p1_jump.png").convert()
-  p1_actions["p1_jump"].set_colorkey((   0,   0,   0))
-  p1_actions["p1_hurt"] = pygame.image.load("images/p1_hurt.png").convert()
-  p1_actions["p1_hurt"].set_colorkey((0, 0, 0))
-  return p1_actions
+    p1_actions = {}
+    # create a dictionary of all player images
+    p1_actions["p1_jump"] = pygame.image.load("images/p1_jump.png").convert()
+    p1_actions["p1_jump"].set_colorkey((   0,   0,   0))
+    p1_actions["p1_hurt"] = pygame.image.load("images/p1_hurt.png").convert()
+    p1_actions["p1_hurt"].set_colorkey((0, 0, 0))
+    return p1_actions
 
 
 def init(p1_actions):
-  global player
-  for i in range(height // 100):
-      for j in range(width // 420):
-          plat = Platforms((random.randint(5, (width - 50) // 10) * 10,  120 * i), 'images/grassHalf.png', 70, 40)
-          platforms.add(plat)
-  player = Player((platforms.sprites()[-1].rect.centerx, platforms.sprites()[-1].rect.centery-300), p1_actions)
-  sprite_list.add(player)
+    global player
+    for i in range(height // 100):
+        for j in range(width // 420):
+            plat = Platforms((random.randint(5, (width - 50) // 10) * 10,  120 * i), 'images/grassHalf.png', 70, 40)
+            platforms.add(plat)
+    player = Player((platforms.sprites()[-1].rect.centerx, platforms.sprites()[-1].rect.centery-300), p1_actions)
+    sprite_list.add(player)
 
 
 def main():
-  global player
-  game_over = False
-  # create platforms
-  p1_actions = get_player_actions()
-  init(p1_actions)
+    global player
+    game_over = False
+    # create platforms
+    p1_actions = get_player_actions()
+    init(p1_actions)
 
-  while True:
-    clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == QUIT:
-          sys.exit()
-        if event.type == KEYDOWN:
-            if event.key == K_f:
-              pygame.display.set_mode(size, FULLSCREEN)
-            if event.key == K_ESCAPE:
-              pygame.display.set_mode(size)
-            # reset the game if it is over by pressing 'R'
-            if game_over and event.key == K_r:
-              player.kill()
-              init(p1_actions)
-              game_over = False
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-      player.left()
-    if keys[pygame.K_RIGHT]:
-      player.right()
+    while True:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_f:
+                    pygame.display.set_mode(size, FULLSCREEN)
+                if event.key == K_ESCAPE:
+                    pygame.display.set_mode(size)
+                # reset the game if it is over by pressing 'R'
+                if game_over and event.key == K_r:
+                    player.kill()
+                    init(p1_actions)
+                    game_over = False
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            player.left()
+        if keys[pygame.K_RIGHT]:
+            player.right()
 
-    if player.update(platforms):
-      game_over = True
-    text = font.render("Score: {}".format(player.progress), True, (255, 0, 0))
-    text_rect = text.get_rect()
-    screen.fill(color)
-    platforms.draw(screen)
-    sprite_list.draw(screen)
-    screen.blit(text, text_rect)
-    pygame.display.flip()
+        if player.update(platforms):
+            game_over = True
+        text = font.render("Score: {}".format(player.progress), True, (255, 0, 0))
+        text_rect = text.get_rect()
+        screen.fill(color)
+        platforms.draw(screen)
+        sprite_list.draw(screen)
+        screen.blit(text, text_rect)
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
-  main()
+    main()
